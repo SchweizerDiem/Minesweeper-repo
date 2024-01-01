e9e8452cd4dc10ee1bb25b8dddc3096a65235a9c
SchweizerDiem <gabrielrochaxtz@gmail.com>
2024-01-01 23:06:39 +0100

    Added function to get number of neighbor bombs

--- domain/cell.py
+++ domain/cell.py
@@ -27,3 +27,18 @@ class Cell:
         if(self.nb_neighbor_bombs == 0):
             return ' '
         return self.nb_neighbor_bombs
+    
+    # get nb of bombs near 
+    # store sum of bombs nearby in 'sum'
+    def get_neighbors_bombs(self): 
+        sum = 0
+        for r in range(max(self.y-1, 0), min(self.y+1, self.dim_size-1)):
+            for c in range(max(self.x-1, 0), min(self.x+1), self.dim_size-1):
+                if self.board[r][c] == '*':
+                    sum +=1
+                else:
+                    continue
+        
+        self.nb_neighbor_bombs = sum
+
+        return self.nb_neighbor_bombs
