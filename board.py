fac8bfcf95faaf844c09c4047976ac8d2a6729b0
SchweizerDiem <gabrielrochaxtz@gmail.com>
2024-01-28 10:33:33 +0000

    Resizing board and adding coordinates.

--- board.py
+++ board.py
@@ -3,17 +3,8 @@ import random
 from domain.cell import Cell
 
 
-# step 1- Create a board and plant the bombs
-# step 2- Put the number of neighbor bombs on the non-bombs cells
-# step 3- Let the user choose a spot on the board to dig
-# step 4- Refresh the board with the new uncovered spots
-# step 5- Repeat step 3/4 until the game ends
-
-# let's create a class for the board
-# and a class to represent the cells in the xadrez board
-
 class Board:
-    def __init__(self, dim_size=10, num_bombs=10):
+    def __init__(self, dim_size=9, num_bombs=10):
         self.dim_size = dim_size
         self.num_bombs = num_bombs
         self.xadrez = [[Cell(x+1, y+1) for y in range(self.dim_size)] for x in range(self.dim_size)] # matrix for objects
@@ -27,11 +18,11 @@ class Board:
         return 'Board(dim_size={}, num_bombs={}, xadrez={})'.format(self.dim_size, self.num_bombs, print_xadrez)
 
     def draw_board(self):
-        drawing = ''
+        drawing = '   A    B    C    D    E    F    G    H    I'
         sep = '\n' + '+----'*self.dim_size + '+\n'
-        
+
         for x in range(self.dim_size):
-            drawing += sep
+            drawing += sep + str(x+1)
             for y in range(self.dim_size):
                 drawing += (f'| {self.xadrez[x][y].draw()} |')
 
@@ -40,7 +31,7 @@ class Board:
     def plant_bombs(self):
         bombs_planted = 0
         while bombs_planted < self.num_bombs:
-            loc = random.randint(0, self.dim_size**2-1) # Chooses a random spot ofthe matrix
+            loc = random.randint(0, self.dim_size**2-1) # Chooses a random spot of the matrix
             row = loc // self.dim_size
             col = loc % self.dim_size
 
@@ -51,6 +42,6 @@ class Board:
                 bombs_planted += 1
 
 
-j1 = Board(5, 5)
+j1 = Board()
 j1.plant_bombs()
 print(j1.draw_board())
